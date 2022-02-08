aad789d17d5e432204ab3389e0f0298b10ca8649
José Pedro Pereira <ze.pereira96@gmail.com>
2022-02-08 21:50:43 +0000

    Solved lab

---
+++ app.js
@@ -0,0 +1,32 @@
+// npm i express
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/home', (request, response, next) => {
+ console.log(request);
+ response.sendFile(__dirname + "/public/views/home.html")
+});
+
+app.get('/about', (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + "/public/views/about.html")
+   });
+
+   app.get('/works', (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + "/public/views/works.html")
+   });
+
+  /*  app.get('/gallery', (request, response, next) => {
+    console.log(request);
+    response.sendFile(__dirname + "/public/views/gallery.html")
+   }); */
+
+
+
+app.listen(3000, () => console.log('Server running on port 3000!'));
